eec5f3ca92a09e3c30f27239f94bd18c2cf94ea2
Sunder Rawat <sunderrawat777@gmail.com>
2021-02-19 21:01:25 +0530

    added 2nd language

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+const getCountry = function(country){
+
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.eu/rest/v2/name/bharat');
+request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
 request.send();
 request.addEventListener('load', function(){
     const [data] = JSON.parse(this.responseText);
@@ -18,7 +20,7 @@ request.addEventListener('load', function(){
       <h3 class="country__name">${data.name}</h3>
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}, ${data.languages[1] === undefined ? '': data.languages[1].name}</p>
       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
     </div>
   </article>
@@ -26,3 +28,8 @@ request.addEventListener('load', function(){
     countriesContainer.insertAdjacentHTML("beforeend", html);
     countriesContainer.style.opacity = 1;
 });
+}
+
+getCountry('bharat');
+getCountry('pakistan');
+getCountry('usa');
