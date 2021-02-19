ec6516ec4aa4e9c2f95ea126ed6010e900882d52
Sunder Rawat <sunderrawat777@gmail.com>
2021-02-19 19:22:01 +0530

    get data from xhr request

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.eu/rest/v2/name/bharat');
+request.send();
+request.addEventListener('load', function(){
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+});
