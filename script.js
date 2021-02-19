6a35503b574ddac5d26d61c72cdf2ddf153b0af4
Sunder Rawat <sunderrawat777@gmail.com>
2021-02-20 00:24:47 +0530

    new feature add neighour country callback hell

--- script.js
+++ script.js
@@ -5,16 +5,43 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountry = function(country){
+//country details
 
-const request = new XMLHttpRequest();
-request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-request.send();
-request.addEventListener('load', function(){
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+// const getCountry = function(country){
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+// request.send();
+// request.addEventListener('load', function(){
+//     const [data] = JSON.parse(this.responseText);
+// //    console.log(data);
+//     const html = `
+//     <article class="country">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}, ${data.languages[1] === undefined ? '': data.languages[1].name}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//     `
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+// });
+// };
+
+// getCountry('bharat');
+// getCountry('pakistan');
+// getCountry('usa');
+
+
+//----------- country and neghbour country------------
+
+const renderCountry = function(data, className =''){
     const html = `
-    <article class="country">
+    <article class="country ${className}">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
@@ -27,9 +54,34 @@ request.addEventListener('load', function(){
     `
     countriesContainer.insertAdjacentHTML("beforeend", html);
     countriesContainer.style.opacity = 1;
-});
 }
 
-getCountry('bharat');
-getCountry('pakistan');
-getCountry('usa');
+const getCountryAndNeighbour = function(country){
+
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+    request.send();
+    request.addEventListener('load', function(){
+        const [data] = JSON.parse(this.responseText);
+     //   console.log(data);
+
+        //render country
+    renderCountry(data);
+
+    //request for neighour country
+    const [neighbour] = data.borders;
+    if(!neighbour) return;
+    console.log(neighbour);
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function(){
+        const data = JSON.parse(this.responseText);
+        //console.log(data);
+        renderCountry(data, 'neighbour');
+    });
+
+});
+};
+
+    getCountryAndNeighbour('bharat');
